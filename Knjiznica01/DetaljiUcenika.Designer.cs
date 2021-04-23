53470e6f6dcf520938809198c7e2acf946daacea
LAPTOP-5HGEC9FV\lucij <bilandzic.lucija@gmail.com>
2021-04-23 18:25:53 +0200

    initial commit

---
+++ Knjiznica01/DetaljiUcenika.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace Knjiznica01
+{
+    partial class DetaljiUcenika
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OIB = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Ime = new System.Windows.Forms.Label();
+            this.Prezime = new System.Windows.Forms.Label();
+            this.Adresa = new System.Windows.Forms.Label();
+            this.Telefon = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.Razred = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OIB
+            // 
+            this.OIB.AutoSize = true;
+            this.OIB.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.OIB.Location = new System.Drawing.Point(55, 42);
+            this.OIB.Name = "OIB";
+            this.OIB.Size = new System.Drawing.Size(43, 28);
+            this.OIB.TabIndex = 0;
+            this.OIB.Text = "OIB";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(156, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 34);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Ime
+            // 
+            this.Ime.AutoSize = true;
+            this.Ime.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Ime.Location = new System.Drawing.Point(55, 101);
+            this.Ime.Name = "Ime";
+            this.Ime.Size = new System.Drawing.Size(44, 28);
+            this.Ime.TabIndex = 2;
+            this.Ime.Text = "Ime";
+            this.Ime.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Prezime
+            // 
+            this.Prezime.AutoSize = true;
+            this.Prezime.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Prezime.Location = new System.Drawing.Point(55, 162);
+            this.Prezime.Name = "Prezime";
+            this.Prezime.Size = new System.Drawing.Size(81, 28);
+            this.Prezime.TabIndex = 3;
+            this.Prezime.Text = "Prezime";
+            // 
+            // Adresa
+            // 
+            this.Adresa.AutoSize = true;
+            this.Adresa.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Adresa.Location = new System.Drawing.Point(55, 221);
+            this.Adresa.Name = "Adresa";
+            this.Adresa.Size = new System.Drawing.Size(72, 28);
+            this.Adresa.TabIndex = 4;
+            this.Adresa.Text = "Adresa";
+            // 
+            // Telefon
+            // 
+            this.Telefon.AutoSize = true;
+            this.Telefon.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Telefon.Location = new System.Drawing.Point(55, 284);
+            this.Telefon.Name = "Telefon";
+            this.Telefon.Size = new System.Drawing.Size(74, 28);
+            this.Telefon.TabIndex = 5;
+            this.Telefon.Text = "Telefon";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(156, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(208, 34);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.Location = new System.Drawing.Point(156, 159);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(208, 34);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox4.Location = new System.Drawing.Point(156, 221);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(335, 34);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox5.Location = new System.Drawing.Point(156, 278);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(208, 34);
+            this.textBox5.TabIndex = 9;
+            // 
+            // Razred
+            // 
+            this.Razred.AutoSize = true;
+            this.Razred.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Razred.Location = new System.Drawing.Point(55, 351);
+            this.Razred.Name = "Razred";
+            this.Razred.Size = new System.Drawing.Size(72, 28);
+            this.Razred.TabIndex = 10;
+            this.Razred.Text = "Razred";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox6.Location = new System.Drawing.Point(156, 351);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(74, 34);
+            this.textBox6.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(389, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 50);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // DetaljiUcenika
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.Razred);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Telefon);
+            this.Controls.Add(this.Adresa);
+            this.Controls.Add(this.Prezime);
+            this.Controls.Add(this.Ime);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.OIB);
+            this.Name = "DetaljiUcenika";
+            this.Text = "DetaljiUcenika";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label OIB;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Ime;
+        private System.Windows.Forms.Label Prezime;
+        private System.Windows.Forms.Label Adresa;
+        private System.Windows.Forms.Label Telefon;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label Razred;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Knjiznica01/DetaljiUcenika.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Knjiznica01
+{
+    public partial class DetaljiUcenika : Form
+    {
+        public DetaljiUcenika()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
